2e9701a2985efdb6e26273488226f366aa4a1093
Jacob Bundgaard <jacob@jacobbundgaard.dk>
2019-04-19 21:32:08 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Firefox Extension: Automatic Dictionary Selection for Spell Checking
+Automatically changes the dictionary used for spell checking based on the language of text in input fields.
+Uses the [Compact Language Detection framework](https://github.com/CLD2Owners/cld2) to determine the text language.
+Icon by A’ Design Award & Competition, Onur Müştak Çobanlı and Farhat Datta through http://www.languageicon.org/
+
+## Tested on sites
+- GitHub
+- Gmail
+- Reddit
+- Stack Exchange
+- Trello
+- Wikipedia
+
+## Content Preferences
+If this extension doesn't work for you for a specific site, you probably have a content preference that overwrites the dictionary for that site.
+This content preference is automatically added if you manually set the dictionary for an input field on a site.
+In order to clear it, try manually setting the dictionary to the same language as the site, e.g., English for Reddit.
+
+If that doesn't work, you can manually clear all content preferences by deleting the `content-prefs.sqlite` in your profile directory.
+On Windows, this can be found in a subfolder of `%AppData%\Mozilla\Firefox\Profiles\`.
+Please note that this will also delete other content preferences you might have set for different sites, such as the zoom level and the default download folder.
---
+++ autodict.js
@@ -0,0 +1,85 @@
+// Firefox doesn't change the dictionary until the element loses focus, then gains it again. This is a workaround.
+function forceDictionaryChange(node) {
+    if (node.nodeName == "INPUT" || node.nodeName == "TEXTAREA") {
+        node.spellcheck = false;
+        node.spellcheck = true;
+    } else {
+        node.contentEditable = false;
+        node.contentEditable = true;
+    }
+}
+
+function getContent(node) {
+    if (node.nodeName == "INPUT" || node.nodeName == "TEXTAREA") {
+        return node.value;
+    } else {
+        return node.innerText;
+    }
+}
+
+async function detectLanguage(event) {
+    const node = event.target;
+    const content = getContent(node);
+
+    //console.log("Detecting language for", node, "with content", content)
+    const detectedLanguage = await browser.i18n.detectLanguage(content);
+    if (detectedLanguage.isReliable && detectedLanguage.languages.length > 0) {
+        const language = detectedLanguage.languages[0].language;
+        if (node.lang !== language) {
+            //console.log("Setting language to", language, "for", node);
+            node.lang = language;
+            forceDictionaryChange(node);
+        }
+    }
+}
+
+function detectChanges(node) {
+    if(!node.spellcheck) {
+        return;
+    }
+
+    if (!(node.nodeName === "INPUT"
+        || node.nodeName === "TEXTAREA"
+        || (node.nodeName === "DIV" && node.contentEditable === "true"))) {
+        return;
+    }
+
+    console.log("Adding listener for", node);
+
+    // TODO: Debounce.
+    node.addEventListener("input", detectLanguage);
+
+    detectLanguage({ target: node });
+}
+
+function detectChildChanges(root) {
+    const nodes = root.querySelectorAll("input, textarea, div[contenteditable]");
+    for (const node of nodes) {
+        detectChanges(node);
+    }
+}
+
+const observer = new MutationObserver(mutations => {
+    for (const mutation of mutations) {
+        switch(mutation.type) {
+            case "attributes":
+                detectChanges(mutation.target);
+                break;
+
+            case "childList":
+                for (const newNode of mutation.addedNodes) {
+                    detectChanges(newNode);
+                    detectChildChanges(newNode);
+                }
+                break;
+        }
+    };
+});
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+    attributeFilter: ["contenteditable"]
+});
+
+detectChildChanges(document);
